58779c1143b2d7564000514c293aa779779965f3
Marine TOURON <marine.touron31@gmail.com>
2021-09-09 16:57:51 +0200

    remplacement des tabulations par des espaces

--- installation_glpi.py
+++ installation_glpi.py
@@ -8,9 +8,10 @@ import shutil
 import os
 import yaml
 
+
 def read_conf(file):
-  with open(file) as f:
-      return yaml.load(f)
+    with open(file) as f:
+        return yaml.load(f)
 
 
 def install_maj(cache):
@@ -42,6 +43,7 @@ def install_paquets(liste_paquets):
             if cache[pack].is_installed:
                 print(pack, "est maintenant installé")
 
+
 def restart_services(service):
     try:
         subprocess.run(['systemctl', 'restart', service])
@@ -50,7 +52,8 @@ def restart_services(service):
     except subprocess.CalledProcessError as e:
         print(e.output)
     else:
-        print((service), "restarted")
+        print(service, "restarted")
+
 
 def create_database(conf):
     mydb = mysql.connector.connect(**conf)
@@ -60,13 +63,14 @@ def create_database(conf):
     except mysql.connector.errors.ProgrammingError:
         print("Access denied")
     else:
-         print("connected, database created")
+        print("connected, database created")
+
 
 def create_user(conf):
     try:
         mydb = mysql.connector.connect(**conf)
 
-        mycursor=mydb.cursor()
+        mycursor = mydb.cursor()
         mycursor.execute("DROP USER IF EXISTS 'glpiuser'@'localhost'")
         mycursor.execute("CREATE USER 'glpiuser'@'localhost' IDENTIFIED BY ''")
         mycursor.execute("grant all privileges on *.* to 'glpiuser'@'localhost'")
@@ -75,7 +79,7 @@ def create_user(conf):
     except mysql.connector.errors.DatabaseError:
         print("User exists")
     else:
-         print("User created")
+        print("User created")
 
 
 def install_glpi(url, path):
@@ -92,17 +96,15 @@ def install_glpi(url, path):
         print("Download sucessfull!")
 
 
-
 def chown(path="/var/www/html/glpi", user='www-data', group=None, recursive=True):
+    """
+    Change user/group ownership of file
+    :param path: path of file or directory
+    :param str user: new owner username
+    :param str group: new owner group name
+    :param bool recursive: set files/dirs recursively
 
     """
-        Change user/group ownership of file
-        :param path: path of file or directory
-        :param str user: new owner username
-        :param str group: new owner group name
-        :param bool recursive: set files/dirs recursively
-
-        """
     try:
         if not recursive or os.path.isfile(path):
             shutil.chown(path, user, group)
@@ -116,11 +118,15 @@ def chown(path="/var/www/html/glpi", user='www-data', group=None, recursive=True
     except OSError as e:
         raise UtilsException(e)
 
+
 def config_glpi():
     try:
-        subprocess.run(['php', '/var/www/html/glpi/bin/console', 'db:install', '-n', '-r', '-f', '-L', 'french', '-d', 'GLPIdb', '-u', 'glpiuser'])
+        subprocess.run(
+            ['php', '/var/www/html/glpi/bin/console', 'db:install', '-n', '-r', '-f', '-L', 'french', '-d', 'GLPIdb',
+             '-u', 'glpiuser'])
     except subprocess.SubprocessError as e:
         print(e.output)
+
 
 def del_file(file):
     try:
@@ -132,30 +138,26 @@ def del_file(file):
     else:
         print("File", (conf['FILEPATH']), "deleted!")
 
+
 conf = read_conf('configuration.yml')
-#Installer les mises à jour
+# Installer les mises à jour
 install_maj(apt.Cache())
-#Installer les différents paquets
+# Installer les différents paquets
 install_paquets(conf['LISTE'])
-#Redémarrer les services Apache2 et mysql
+# Redémarrer les services Apache2 et mysql
 restart_services('apache2')
 restart_services('mysql')
-#Création de la base de données
+# Création de la base de données
 create_database(conf['CONFIG'])
-#Création de l'utilisateur
+# Création de l'utilisateur
 create_user(conf['CONFIG'])
-#Installation de GLPI
+# Installation de GLPI
 install_glpi(conf['URL'], conf['PATH'])
-#Attribution des droits d'accès
+# Attribution des droits d'accès
 chown()
-#Configuration de GLPI
+# Configuration de GLPI
 config_glpi()
-#Attribution des droits d'accès
+# Attribution des droits d'accès
 chown()
-#Suppression du fichier "install.php"
+# Suppression du fichier "install.php"
 del_file(conf['FILEPATH'])
-
-
-
-
-
